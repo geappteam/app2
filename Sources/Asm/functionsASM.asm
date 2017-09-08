652efb983afb90e3026f50e8f889c236ddebabce
dene2303 <dene2303@LAB3125-GEL11.usherbrooke.ca>
2017-09-08 14:23:18 -0400

    Clean build

--- Sources/Asm/functionsASM.asm
+++ Sources/Asm/functionsASM.asm
@@ -1,8 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;; Les directives ;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	.def _Addition8IntASM
-	.def _AdditionFloatASM
+
 
 	.data
 
@@ -12,34 +11,5 @@
 _Addition8IntASM
     .asmfunc
 
-	ADD	.L1	A4,B4,A4
-	ADD	.L1	A4,A6,A4
-	ADD	.L1	A4,B6,A4
-	ADD	.L1	A4,A8,A4
-	ADD	.L1	A4,B8,A4
-	ADD	.L1	A4,A10,A4
-	ADD	.L1	A4,B10,A4
-
-    B B3 ; INDISPENSABLE ; B3 contient l'adresse de retour
-    NOP 5
 
     .endasmfunc
-
-_AdditionFloatASM
-    .asmfunc
-
-	MVKL 0x3f000000, B4
-	MVKH 0x3f000000, B4
-	ADDSP .L1 A4,B4,A4
-
-    B B3 ; INDISPENSABLE ; B3 contient l'adresse de retour
-    NOP 5
-
-    .endasmfunc
-
-_AddSatureASM
-	.asmfunc
-
-	ADD	.L1	A4,B4,A4
-
-	.endasmfunc
