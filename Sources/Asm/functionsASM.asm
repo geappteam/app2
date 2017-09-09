c96d81a8da9cb409993af0094f701e1d4a294494
Lilian Racault <racl2605@usherbrooke.ca>
2017-09-09 18:13:04 -0400

    Rajoute fonction assembleur

--- Sources/Asm/functionsASM.asm
+++ Sources/Asm/functionsASM.asm
@@ -4,6 +4,9 @@
 	.def _AddEntierNonSigne32bits
 	.def _AddEntierSigne32bits
 	;.def _AddFractionnaire32bits_Q7.24_Q15.16
+	;.def _SubEntierNonSigne32bits
+	.def _SubEntierSigne32
+	.def _SubFlottant64bits
 	.data
 
 
@@ -16,7 +19,11 @@ _AddEntierNonSigne32bits
     LDW *+A4[0],B4
     NOP 4
 
-    SADD A3, B4, B5
+    ADDU A3, B4, A5:A4
+	OR B0, A5, B0
+    [B0] CLR A4,A4, A5
+    [B0] MVKL 0xFFFFFFFF, A4
+
 
     B B3
     NOP 5
@@ -31,7 +38,14 @@ _AddEntierSigne32bits
     LDW *+A4[0],B4
     NOP 4
 
+
+
     SADD A3, B4, B5
+
+    NOP 1
+
+    ;MVC CSR, B6
+
 
 
     B B3
@@ -50,20 +64,66 @@ _AddEntierSigne32bits
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-_SubEntierNonSigne32bits
+;_SubEntierNonSigne32bits
+;	.asmfunc
+
+;	LDW *+A4[1],A3
+;    LDW *+A4[0],B4
+;    NOP 4
+
+;	SUB A3, B4, B7:B6
+
+
+;	B B3
+;    NOP 5
+
+;    .endasmfunc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_SubEntierSigne32
 	.asmfunc
 
 	LDW *+A4[1],A3
     LDW *+A4[0],B4
     NOP 4
 
-	SUB A3, B4, B10:B11
+	SSUB A3, B4, B6
 
 
 	B B3
     NOP 5
 
     .endasmfunc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_SubFlottant64bits
+	.asmfunc
+
+	LDW *+A4[1],B0
+    LDW *+A4[0],A3
+    NOP 4
+
+	SUBDP B1:B0,A3:A2,A5:A4
+
+
+	B B3
+    NOP 5
+
+    .endasmfunc
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Sources/C/app2.c
+++ Sources/C/app2.c
@@ -1,16 +1,34 @@
 #include "Cfunctions.h"
 
+
+double AddEntierNonSigne32bits(unsigned int *TabIntNoS);
+double AddEntierSigne32bits( int *TabIntS);
+double SubEntierSigne32 (int *TabIntNoS);
+double SubFlottant64bits(float *TabXXX);
+
 int main(void)
 {
     //Temporarily static variables instead of inputs from user
-    dataType = 1;
-    dataFormat = 1;
-    op = 1;
+    //dataType = 1;
+    //dataFormat = 1;
+    //op = 1;
 
-    ObtenirType(Liste);
-    ObtenirFormat(Liste);
-    ObtenirOperation(Liste);
+    //ObtenirType(Liste);
+    //ObtenirFormat(Liste);
+    //ObtenirOperation(Liste);
  //   AnalyserListe(Liste,TabShortNoS,TabShortS);
+
+    //unsigned int param[] = { 3, 0x7};
+    //int param1[] = { 0x25454546, 0x5FFFFFFF};
+    //int param2[] = { 0x802A3FA2,0x5A2E51A3 };
+    float param3[] = { 3.5,13.5 };
+
+
+
+    //double result = AddEntierNonSigne32bits(param);
+    //double result1 = AddEntierSigne32bits(param1);
+    //double result2 = SubEntierSigne32(param2);
+    float result3= SubFlottant64bits(param3);
 
     return 0;
 
