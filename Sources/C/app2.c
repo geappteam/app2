6aa7b888ef4d0b27f35c9a911aa3226210a33b36
MoronixProduct3 <moronicproduct3@gmail.com>
2017-09-09 19:18:20 -0400

    Update app2.c

--- Sources/C/app2.c
+++ Sources/C/app2.c
@@ -1,19 +1,14 @@
 #include "Cfunctions.h"
 
-<<<<<<< HEAD
-
 double AddEntierNonSigne32bits(unsigned int *TabIntNoS);
 double AddEntierSigne32bits( int *TabIntS);
 double SubEntierSigne32 (int *TabIntNoS);
 double SubFlottant64bits(float *TabXXX);
-=======
 long SubEntierNonSigne32bits(unsigned int *TabIntNoS);
->>>>>>> 9ae696efe24bae88dc0d3a34d19ad5b25d3cc874
 
 int main(void)
 {
     //Temporarily static variables instead of inputs from user
-<<<<<<< HEAD
     //dataType = 1;
     //dataFormat = 1;
     //op = 1;
@@ -22,7 +17,6 @@ int main(void)
     //ObtenirFormat(Liste);
     //ObtenirOperation(Liste);
  //   AnalyserListe(Liste,TabShortNoS,TabShortS);
-=======
 //    dataType = 1;
 //    dataFormat = 2;
 //    op = 1;
@@ -31,7 +25,6 @@ int main(void)
 //    ObtenirFormat(Liste);
 //    ObtenirOperation(Liste);
 //    AnalyserListe(Liste,TabShortNoS,TabShortS);
->>>>>>> 9ae696efe24bae88dc0d3a34d19ad5b25d3cc874
 
     //unsigned int param[] = { 3, 0x7};
     //int param1[] = { 0x25454546, 0x5FFFFFFF};
@@ -46,5 +39,4 @@ int main(void)
     float result3= SubFlottant64bits(param3);
 
     return 0;
-
 }
